22a268ce0be25b1df0b871630531238331665376
Ari <ariannasg.ldn@gmail.com>
2020-06-02 16:00:04 +0100

    Added pagination to the product list API view

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -3,9 +3,15 @@ from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import LimitOffsetPagination
 
 from store.models import Product
 from store.serializers import ProductSerializer
+
+
+class ProductsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 
 
 # The generic views in Django REST framework will cover what you need from a
@@ -13,7 +19,6 @@ from store.serializers import ProductSerializer
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
     # we add the ability to filter products using URL query parameters
     filter_backends = (DjangoFilterBackend, SearchFilter)
     # this is used by the DjangoFilterBackend to filter products by ID
@@ -21,6 +26,13 @@ class ProductList(ListAPIView):
     # this is used by the SearchFilter backend to map from the URL query
     # parameters, to the model fields of the serialized model
     search_fields = ('name', 'description')
+    # using offset pagination
+    # now the api result will include things like:
+    #   "count": 4,
+    #   "next": "http://127.0.0.1:8000/api/v1/products?limit=1&offset=2",
+    #   "previous": "http://127.0.0.1:8000/api/v1/products?limit=1",
+    #   "results": [ ... ]
+    pagination_class = ProductsPagination
 
     # we are also to filter products by whether they are on sale or not
     def get_queryset(self):
