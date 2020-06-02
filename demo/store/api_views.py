c08475eda2ffc2df261e8747c6f03ea7bfeebfa8
Ari <ariannasg.ldn@gmail.com>
2020-06-02 15:49:17 +0100

    Enabled Product full text search based on name and description using the SearchFilter backend

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python3
 from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
 
 from store.models import Product
@@ -14,9 +15,12 @@ class ProductList(ListAPIView):
     serializer_class = ProductSerializer
 
     # we add the ability to filter products using URL query parameters
-    filter_backends = (DjangoFilterBackend,)
-    # we are going to filter products by whether they match a specific ID
-    filter_fields = ('id', )
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    # this is used by the DjangoFilterBackend to filter products by ID
+    filter_fields = ('id',)
+    # this is used by the SearchFilter backend to map from the URL query
+    # parameters, to the model fields of the serialized model
+    search_fields = ('name', 'description')
 
     # we are also to filter products by whether they are on sale or not
     def get_queryset(self):
