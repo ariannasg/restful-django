c638b96e42e5e85ed044fa01b33faa526e330374
Ari <ariannasg.ldn@gmail.com>
2020-06-02 14:50:32 +0100

    Connect the product list API view to a URL route

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -3,11 +3,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+import store.api_views
 import store.views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/<int:product_id>/', store.views.show, name='show-product'),
-    path('cart/', store.views.cart, name='shopping-cart'),
-    path('', store.views.index, name='list-products'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = \
+[
+        path('api/v1/products', store.api_views.ProductList.as_view()),
+
+        path('admin/', admin.site.urls),
+
+        path('products/<int:product_id>/', store.views.show,
+             name='show-product'),
+        path('cart/', store.views.cart, name='shopping-cart'),
+        path('', store.views.index, name='list-products'),
+    ] \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
