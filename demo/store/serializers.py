638a1cf1d9a065b068dd13840fcddaba25605cc7
Ari <ariannasg.ldn@gmail.com>
2020-06-02 17:33:56 +0100

    Using serializer fields instead of the to_representation method on the serializer for serializing custom fields

--- demo/store/serializers.py
+++ demo/store/serializers.py
@@ -4,14 +4,16 @@ from store.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    # to simplify how we added custom field data, we can make the attributes
+    # that we initially set in the two representation method, to use serializer
+    # fields.
+    is_on_sale = serializers.BooleanField(read_only=True)
+    current_price = serializers.FloatField(read_only=True)
+    # override the description field by adding some props to it for adding
+    # validation
+    description = serializers.CharField(min_length=2, max_length=100)
+
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'price', 'sale_start',
-                  'sale_end')
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data['is_on_sale'] = instance.is_on_sale()
-        data['current_price'] = instance.current_price()
-
-        return data
+        fields = ('id', 'name', 'description', 'price',
+                  'sale_start', 'sale_end', 'is_on_sale', 'current_price')
